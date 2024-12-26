68395ed1b3c748195b3eb37504c25a60ce79dd44
XIAOWEI1234455 <sel340569@gmail.com>
2024-12-26 20:49:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的智能家居服务中心', url: 'https://blog.tangly1024.com' },
-    { title: '访问技术分享获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问智能家居系列获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
